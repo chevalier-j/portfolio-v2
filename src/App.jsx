39eedf61d116477fb56385d2c967281818934846
J.Gibert <adelgibert@gmail.com>
2023-07-22 17:37:44 +1200

    removing unused imports and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
-import { Test } from '@common/test'
-import { Navbar } from '@common/navbar'
+// import { Navbar } from '@common/navbar'
 
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Test />
 			<div className="relative z-0 bg-primary">
-				<Navbar />
+				{/* <Navbar /> */}
 				<header className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></header>
 			</div>
 		</BrowserRouter>
