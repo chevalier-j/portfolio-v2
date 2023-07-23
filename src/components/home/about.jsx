1345dd14cc303588ecd56e490e0667d927a13160
J.Gibert <adelgibert@gmail.com>
2023-07-23 19:28:44 +1200

    about component

---
+++ src/components/home/about.jsx
@@ -0,0 +1,55 @@
+import Tilt from 'react-parallax-tilt'
+import { delay, motion } from 'framer-motion'
+import { styles } from '@styles'
+import { services } from '@constants'
+import { fadeIn, textVariant } from '@utils/motion'
+const ServiceCard = ({ title, icon, index }) => {
+	return (
+		<Tilt className="xs:w-[250px] w-full">
+			<motion.div
+				variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+				className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+			>
+				<div
+					options={{ max: 45, scale: 1, speed: 450 }}
+					className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
+				>
+					<img
+						src={icon}
+						alt={title}
+						className="w-16 h-16 object-contain"
+					/>
+					<h3 className="text-white text-[20px] font-bold text-center">
+						{title}
+					</h3>
+				</div>
+			</motion.div>
+		</Tilt>
+	)
+}
+
+const About = () => {
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview</h2>
+			</motion.div>
+
+			<motion.p
+				variants={fadeIn('', '', 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+			>
+				Blahblah description about me etc Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Dicta corporis atque laboriosam eaque
+				sed dolores possimus architecto, saepe eum rem molestiae obcaecati
+				pariatur culpa quas temporibus impedit nobis. Perferendis, nihil.
+			</motion.p>
+			<div className="mt-20 flex flex-wrap gap-10">
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
+			</div>
+		</>
+	)
+}
