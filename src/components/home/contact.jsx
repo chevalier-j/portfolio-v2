c14d593a338e7b6e9203616eb1463565dcf3fcde
J.Gibert <adelgibert@gmail.com>
2023-07-24 20:41:56 +1200

    state, ref, function scaffolding, import fixed

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -1,13 +1,18 @@
 import { useState, useRef } from 'react'
 import { motion } from 'framer-motion'
-import { emailjs } from '@emailjs/browser'
+import emailjs from '@emailjs/browser'
 import { styles } from '@styles'
-import { EarthCanvas } from '@canvas'
+// import { EarthCanvas } from '@canvas/'
 import { slideIn } from '@utils/motion'
 import { SectionWrapper } from '@utils'
 
 const Contact = () => {
 	const [form, setForm] = useState({ name: '', email: '', message: '' })
+	const formRef = useRef()
+	const [loading, setLoading] = useState(false)
+
+	const handleChange = e => {}
+	const handleSubmit = e => {}
 	return <h1>Contact</h1>
 }
 
