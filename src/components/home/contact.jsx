f9edd3d668c899b55a07235d3d4f1db429a5f0b3
J.Gibert <adelgibert@gmail.com>
2023-07-24 20:33:44 +1200

    imports

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -1,3 +1,8 @@
+import { motion } from 'framer-motion'
+import { emailjs } from '@emailjs/browser'
+import { styles } from '@styles'
+import { EarthCanvas } from '@canvas'
+import { slideIn } from '@utils/motion'
 import { SectionWrapper } from '@utils'
 
 const Contact = () => {
