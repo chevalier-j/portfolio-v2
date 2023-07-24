46f1538db1ba62f51cea01dd26ebf50927bc742b
J.Gibert <adelgibert@gmail.com>
2023-07-24 20:59:41 +1200

    title and subtitle

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -17,9 +17,10 @@ const Contact = () => {
 		<div className="xl:mt-12 xl:flex-row flex flex-col-reverse gap-10 overflow-hidden">
 			<motion.div
 				variants={slideIn('left', 'tween', 0.2, 1)}
-				className="flex-[0.75]"
+				className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
 			>
-				Contact
+				<p className={styles.sectionSubText}>Get in touch</p>
+				<h2 className={styles.sectionHeadText}>Contact.</h2>
 			</motion.div>
 		</div>
 	)
