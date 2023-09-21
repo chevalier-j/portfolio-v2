fa7e71ebc3ec56712f18c31dc47fd9f6eb36c8da
J.Gibert <adelgibert@gmail.com>
2023-09-22 09:57:34 +1200

    inversed direction of slide ins

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
 		} catch (error) {
 			setLoading(false)
 			console.log(error)
-			alert('Ouch! Something went wrong :(')
+			alert('Something went wrong 😩')
 		}
 	}
 
@@ -79,7 +79,7 @@ const Contact = () => {
 			className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
 		>
 			<motion.div
-				variants={slideIn('left', 'tween', 0.2, 1)}
+				variants={slideIn('right', 'tween', 0.1, 1)}
 				className={`flex-[0.75] bg-black-100 p-8 rounded-2xl`}
 			>
 				<p className={`${styles.sectionSubText}`}>Get in touch</p>
@@ -162,7 +162,7 @@ const Contact = () => {
 				</form>
 			</motion.div>
 			<motion.div
-				variants={slideIn('right', 'tween', 0.2, 1)}
+				variants={slideIn('left', 'tween', 0.01, 1)}
 				className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px]`}
 			>
 				<PlanetCanvas />
